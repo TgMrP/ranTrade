eefea443890b794fa64c33ea42b3b24403ea23f3
Ran Tayar <tgmrp@Rans-MacBook-Pro.local>
2022-03-09 15:15:23 +0200

    add axios requset to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="text-8xl text-trade-orange">בדיקה</div>
+  <div class="text-8xl text-trade-orange">
+    {{ data }}
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ $axios }) {
+    const data = await $axios.$get('/api')
+
+    return {
+      data,
+    }
+  },
 }
 </script>
