b3f69cb1e69fddb8edf62411fca4e253f38219e3
Ran Tayar <tgmrp@Rans-MacBook-Pro.local>
2022-03-09 14:59:28 +0200

    setup basic express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      'img-src': ["'self'", 'data:'],
+    },
+  },
+}))
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.all('/', (req, res) => {
   res.json({ data: 'data' })
 })
